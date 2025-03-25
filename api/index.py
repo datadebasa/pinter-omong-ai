4cbde0876655ee4a98921b55d1ea6d4934051365
sodikinnaa <sodikinnaa@gmail.com>
2025-03-26 05:59:42 +0700

    push data finish

--- api/index.py
+++ api/index.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template,request, jsonify
+import requests
 
 app = Flask(__name__)
-
+def chatbot(promt):
+    url = f'https://open-source-backend.vercel.app/podcast-ai?prompt={promt}'
+    response = requests.get(url)
+    response = response.json()
+    return response
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/pinter-omong')
+def pinter_omong():
+    return render_template('chat.html')
+
+@app.route('/pinter-omong/chat')
+def chat():
+    promt = request.args.get('prompt')
+    response = chatbot(promt)
+    response = response['payload']
+    
+    return jsonify(response)
+
+    
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
